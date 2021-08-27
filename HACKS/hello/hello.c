cbc7bc2c93b92a0dde11fec976e5310d5a62c75d
Stephanny Velasquez <svelasquez4@huskers.unl.edu>
2021-08-27 15:31:09 +0000

    add major to the output

---
+++ HACKS/hello/hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Stephanny Velasquez
+ *         svelasquez4@huskers.unl.edu
+ * Date: 2021/08/27
+ *
+ * Prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Stephanny Velasquez!!\n");
+  printf("I am working on a a Physics degree with a minor in Computer Science.\n");
+
+  return 0;
+}
